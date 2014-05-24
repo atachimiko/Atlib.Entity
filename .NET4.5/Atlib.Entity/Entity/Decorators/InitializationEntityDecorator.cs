459e0f421c9286345fdddaf42ef7e207c51fb27b
atachimiko <atachi.miko@gmail.com>
2014-05-24 15:01:48 +0900

    Add

---
+++ .NET4.5/Atlib.Entity/Entity/Decorators/InitializationEntityDecorator.cs
@@ -0,0 +1,39 @@
+﻿using Atlib.Attributes;
+using log4net;
+using System;
+using System.Text;
+using System.Reflection;
+using ImpromptuInterface;
+
+namespace Atlib.Entity.Decorators
+{
+	/// <summary>
+	///     EntityInitialization属性が付与してある場合、初期化処理を実行します。
+	/// </summary>
+	public class InitializationEntityDecorator : BusinessLogicDecorator
+	{
+		static ILog LOG = LogManager.GetLogger(typeof(InitializationEntityDecorator));
+
+		public InitializationEntityDecorator(IEntityData data)
+		{
+			this._Entity = data;
+		}
+
+		public object[] Arguments { get; set; }
+
+		public override void Operation()
+		{
+			base.Operation();
+
+			var clazz = _Entity.GetType();
+
+			var attribute = clazz.GetCustomAttribute<EntityInitializationAttribute>();
+			if (attribute == null) return; // EntityInitialization属性が無い場合はそのまま終了。
+
+			var validator = Activator.CreateInstance(attribute.Initializer);
+			validator.ActLike<IEntityInitialization>().Initialization(this._Entity, this.Arguments);
+		}
+
+		readonly IEntityData _Entity;
+	}
+}
---
+++ .NET4.5/Atlib.Entity/Entity/IEntityInitialization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlib.Entity
+{
+	public interface IEntityInitialization
+	{
+		void Initialization(IEntityData entity, params object[] args);
+	}
+}
