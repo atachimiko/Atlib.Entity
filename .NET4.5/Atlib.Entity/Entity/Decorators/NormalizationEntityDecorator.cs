d432059b3339083417063f714cba2d0d61e156e4
atachimiko <atachi.miko@gmail.com>
2014-05-28 14:44:14 +0900

    Fix

---
+++ .NET4.5/Atlib.Entity/Entity/Decorators/NormalizationEntityDecorator.cs
@@ -0,0 +1,45 @@
+﻿using Atlib.Attributes;
+using log4net;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImpromptuInterface;
+
+namespace Atlib.Entity.Decorators
+{
+	/// <summary>
+	///     
+	/// </summary>
+	public class NormalizationEntityDecorator : BusinessLogicDecorator
+	{
+		static ILog LOG = LogManager.GetLogger(typeof(NormalizationEntityDecorator));
+
+		public NormalizationEntityDecorator(IEntityData data)
+		{
+			this._Entity = data;
+		}
+
+		public object[] Arguments { get; set; }
+
+		public override void Operation()
+		{
+			base.Operation();
+
+			var clazz = _Entity.GetType();
+			foreach (var prop in clazz.GetProperties())
+			{
+				var attr = prop.GetCustomAttribute<EntityNormalizationAttribute>();
+				if (attr != null)
+				{
+					var normalizer = Activator.CreateInstance(attr.Normalization);
+					normalizer.ActLike<IEntityNormalization>().Normalization(this._Entity, prop, Arguments);
+				}
+			}
+		}
+
+		readonly IEntityData _Entity;
+	}
+}
